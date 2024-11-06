73c8a81759b7dee570bb60c4b3c2840e08f38dee
grogiewicz <gabrysia.rogiewicz@gmail.com>
2024-11-06 15:08:58 +0100

    more_advanced_code

--- helpers/data.js
+++ helpers/data.js
@@ -1,2 +1,5 @@
+import 'dotenv/config'
 export const userID = "f0373e1b-f3c7-4711-8c9e-6e82b10e55a3"
 export const baseUrl = "https://demoqa.com/"
+export const user = "testgr"
+export const password = process.env.PASSWORD
--- tests/api.test.js
+++ tests/api.test.js
@@ -2,10 +2,12 @@ import {expect} from "chai"
 import pkg from 'pactum';
 const {spec} = pkg;
 import 'dotenv/config'
-import { baseUrl, userID } from "../helpers/data.js"
+import { baseUrl, password, userID, user } from "../helpers/data.js"
+
+let token_response;
 
 describe("Api tests", () =>{
-    it("get request", async () => {
+    it.skip("get request", async () => {
         const response = await spec()
         .get (`${baseUrl}BookStore/v1/Books`)
         .inspect()
@@ -23,9 +25,69 @@ describe("Api tests", () =>{
         .post(`${baseUrl}Account/v1/User`)
         .withBody({
             userName: "testgr",
-            password: process.env.PASSWORD,
+            password: password,
         })
         .inspect()
         expect(response.statusCode).to.eql(201)
+    })
+
+    it("Generate token", async () => {
+        const response = await spec()
+        .post(`${baseUrl}Account/v1/GenerateToken`)
+        .withBody({
+            userName: user,
+            password: password,
+        })
+        .inspect()
+        token_response = response.body.token
+        console.log(token_response)
+        expect(response.statusCode).to.eql(200)
+        expect(response.body.result).to.eql("User authorized successfully.")
+
+    })
+
+    //it("Check token", async () => {
+    //    console.log("another block", token_response)
+    //})
+
+    it.skip("Add book", async () => {
+        const response = await spec()
+        .post(`${baseUrl}Bookstore/v1/Books`)
+        .withBearerToken(token_response)
+        .withBody({
+            userId: userID,
+            collectionOfIsbns: [
+            {
+                isbn : "9781491904244"
+            }
+        ]
+        })
+        .inspect()
+        expect(response.statusCode).to.eql(201)
+    })
+
+    it("Check book for user", async () => {
+        const response = await spec()
+        .get(`${baseUrl}Account/v1/User/${userID}`)
+        .inspect()
+        .withBearerToken(token_response)
+        expect(response.statusCode).to.eql(200)
+    })
+
+    it("Delete all books for user", async() => {
+        const response = await spec()
+        .delete(`${baseUrl}BookStore/v1/Books/${userID}`)
+        .inspect()
+        .withBearerToken(token_response)
+        expect(response.statusCode).to.eql(204)
+    })
+
+    it("Check book for user", async () => {
+        const response = await spec()
+        .get(`${baseUrl}Account/v1/User/${userID}`)
+        .inspect()
+        .withBearerToken(token_response)
+        expect(response.statusCode).to.eql(200)
+        expect(response.body.books).to.eql/([])
     })
 })
